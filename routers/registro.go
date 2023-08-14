67707c1de9d29fa01f684bc2fde5b5d62971e939
auladeproyectos <auladeproyectos@gmail.com>
2023-08-13 19:40:07 -0500

    Ultimo commit

---
+++ routers/registro.go
@@ -0,0 +1,64 @@
+package routers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/Auladeproyectos/TwitterGo/bd"
+	"github.com/Auladeproyectos/TwitterGo/models"
+)
+
+func Registro(ctx context.Context) models.RespApi {
+	var t models.Usuario
+	var r models.RespApi
+	r.Status = 400
+	fmt.Println("Entre a Registro")
+
+	body := ctx.Value(models.Key("body")).(string)
+	err := json.Unmarshal([]byte(body), &t)
+	if err != nil {
+		r.Message = err.Error()
+		fmt.Println(r.Message)
+		return r
+	}
+
+	if len(t.Email) == 0 {
+		r.Message = "Debe especificar el Email"
+		fmt.Println(r.Message)
+		return r
+	}
+	if len(t.Password) < 6 {
+		r.Message = "Debe especificar una contraseña de al menos 6 caracteres"
+		fmt.Println(r.Message)
+		return r
+	}
+
+	_, encontrado, _ := bd.ChequeoYaexisteUsuario(t.Email)
+
+	if encontrado {
+		r.Message = "Ya existe un usuario registrado con ese mismo email"
+		fmt.Println(r.Message)
+		return r
+	}
+
+	_, status, err := bd.InsertoRegistro(t)
+
+	if err != nil {
+		r.Message = "Ocurrio un error al intentar realizar el registro de usuario" + err.Error()
+		fmt.Println(r.Message)
+		return r
+	}
+
+	if !status {
+		r.Message = "No se ha logrado insertar el registro del usuario"
+		fmt.Println(r.Message)
+		return r
+	}
+
+	r.Status = 200
+	r.Message = "Registro Ok"
+	fmt.Println(r.Message)
+	return r
+
+}
